0c81146ebb460960e627b689bd9b8b7debc3140e
Eun Byul Cheon <75746836+eunbyulcheon@users.noreply.github.com>
2023-03-30 20:42:37 +0900

    Modified: Navigator route

---
+++ lib/features/main_navigation/main_nav.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainNav extends StatelessWidget {
+  const MainNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
