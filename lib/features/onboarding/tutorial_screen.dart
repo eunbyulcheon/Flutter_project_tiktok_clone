338f0980b18498e3f3e99b98e7a3164e1bbf3cd5
Eun Byul Cheon <75746836+eunbyulcheon@users.noreply.github.com>
2023-03-30 19:01:39 +0900

    "Onboarding interests screen"
    - When scrolled down, the App bar's title disappears.
    - Allows user to select items from the interest list.

---
+++ lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
