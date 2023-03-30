ab7b007cf0c589a82fd5a860a75687f2737a7629
Eun Byul Cheon <75746836+eunbyulcheon@users.noreply.github.com>
2023-03-30 20:12:00 +0900

    'Tutorial Screen'
    - The AnimatedCrossFade widget uses the returned values of onPanUpdate and onPanEnd to display between two screens.

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, second }
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,8 +15,109 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showingpage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showingpage = Page.second;
+      });
+    } else {
+      setState(() {
+        _showingpage = Page.first;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+              firstChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Gaps.v40,
+                  Text(
+                    'Swipe',
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    'Videos are personalized for you based on what you watch, like, and share.',
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                  Gaps.v56,
+                ],
+              ),
+              secondChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Gaps.v40,
+                  Text(
+                    'Follow the rules',
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    'Take care of one another.',
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                  Gaps.v56,
+                ],
+              ),
+              crossFadeState: _showingpage == Page.first
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: const Duration(milliseconds: 300),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: const EdgeInsets.all(
+              Sizes.size24,
+            ),
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: _showingpage == Page.first ? 0 : 1,
+              child: CupertinoButton(
+                onPressed: () {},
+                color: Theme.of(context).primaryColor,
+                child: const Text('Enter the App'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
