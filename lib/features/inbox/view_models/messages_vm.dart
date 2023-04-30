8e86a03e72b0b69181fb08fc2cdb993d3cb06cfe
Eun Byul Cheon <75746836+eunbyulcheon@users.noreply.github.com>
2023-04-30 15:31:16 +0900

    'Add: Retrieving message from database'
    - Used a Stream Provider to return a stream (a list of message model).

--- lib/features/inbox/view_models/messages_vm.dart
+++ lib/features/inbox/view_models/messages_vm.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/authentication/repos/auth_repo.dart';
 import 'package:tiktok_clone/features/inbox/models/message_model.dart';
@@ -21,6 +22,7 @@ class MessagesViewModel extends AsyncNotifier<void> {
         final message = MessageModel(
           text: text,
           userId: user!.uid,
+          createdAt: DateTime.now().millisecondsSinceEpoch,
         );
         _repository.sendMessage(message);
       },
@@ -30,4 +32,26 @@ class MessagesViewModel extends AsyncNotifier<void> {
 
 final messagesProvider = AsyncNotifierProvider<MessagesViewModel, void>(
   () => MessagesViewModel(),
+);
+
+final chatProvider = StreamProvider<List<MessageModel>>(
+  (ref) {
+    final db = FirebaseFirestore.instance;
+
+    return db
+        .collection('chat_rooms')
+        .doc('27YtMTTPqmx72vGcmcmK')
+        .collection('texts')
+        .orderBy('createdAt')
+        .snapshots()
+        .map(
+          (event) => event.docs
+              .map(
+                (doc) => MessageModel.fromJson(
+                  doc.data(),
+                ),
+              )
+              .toList(),
+        );
+  },
 );
